90f5411b0082292d19e9cc40e6a6a5d328b8fc68
Tashrik Anam <anampartho31@gmail.com>
2023-05-23 13:34:00 +0600

    :heavy_plus_sign: Add session functionality

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,8 +1,37 @@
 import bcrypt from "bcryptjs";
-import { json } from "@remix-run/node";
+import { redirect, createCookieSessionStorage, json } from "@remix-run/node";
 import type { LoginForm, RegistrationForm } from "./types.server";
 import { prisma } from "./prisma.server";
 import { createUser } from "./user.server";
+
+// Session
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "kudos-session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirecTo: string) {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+
+  return redirect(redirecTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
+}
 
 export async function register(user: RegistrationForm) {
   const exists = await prisma.user.count({ where: { email: user.email } });
@@ -25,6 +54,8 @@ export async function register(user: RegistrationForm) {
       { status: 400 }
     );
   }
+
+  return createUserSession(newUser.id, "/");
 }
 
 export async function login({ email, password }: LoginForm) {
@@ -34,5 +65,5 @@ export async function login({ email, password }: LoginForm) {
     return json({ error: `Incorrect login` }, { status: 400 });
   }
 
-  return { id: user.id, email };
+  return createUserSession(user.id, "/");
 }
