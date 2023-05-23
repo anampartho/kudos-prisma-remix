3bf8d2dbede8c79612553e34c0317499355c1a5f
Tashrik Anam <anampartho31@gmail.com>
2023-05-24 00:50:25 +0600

    :hear_no_evil: Show users on side panel

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,24 @@
-import { LoaderFunction } from "@remix-run/node";
-import { requireUserId } from "~/utils/auth.server";
+import { LoaderFunction, json } from "@remix-run/node";
+import { getOtherUsers, requireUserId } from "~/utils/auth.server";
 import { Layout } from "~/components/layout";
 import { UserPanel } from "~/components/user-panel";
+import { useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserId(request);
-  return null;
+  const userId = await requireUserId(request);
+  const users = await getOtherUsers(userId);
+
+  return json({ users });
 };
 
 export default function Home() {
+  const { users } = useLoaderData();
+
   return (
     <Layout>
       <div className="h-full flex">
-        <UserPanel />
+        <UserPanel users={users} />
+        <div className="flex-1"></div>
       </div>
     </Layout>
   );
