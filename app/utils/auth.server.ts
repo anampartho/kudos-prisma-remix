82853c52c5c8b99533035f75266423faabfb6924
Tashrik Anam <anampartho31@gmail.com>
2023-05-23 23:42:50 +0600

    :lock: Protect routes by checking users session

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -22,6 +22,59 @@ const storage = createCookieSessionStorage({
   },
 });
 
+export async function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    throw redirect(`/login?${searchParams}`);
+  }
+
+  return userId;
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+
+  if (!userId || typeof userId !== "string") return null;
+
+  return userId;
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+
+  return redirect("/login", {
+    headers: { "Set-Cookie": await storage.destroySession(session) },
+  });
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+
+  if (typeof userId !== "string") return null;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, email: true, profile: true },
+    });
+
+    return user;
+  } catch (error) {
+    throw logout(request);
+  }
+}
+
 export async function createUserSession(userId: string, redirecTo: string) {
   const session = await storage.getSession();
   session.set("userId", userId);
